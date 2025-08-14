66106c79ac6ccdbea6b65d85ba4c1e5009e50950
FabioStorer <fabiostorer19@gmail.com>
2025-08-14 20:49:35 -0300

    zod funcional

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,39 @@
 import express from 'express';
 import { Request, Response } from 'express';
 import 'dotenv/config.js';
-import { validate, mask } from 'validation-br/dist/cpf'
-const cpf = require('validation-br/dist/cpf')
+import { validate, mask, fake } from 'validation-br/dist/cpf';
+import z from 'zod';
+
+const cpf = require('validation-br/dist/cpf');
 
 const app = express();
 
 app.use(express.json());
 
-const fakecpf: number = cpf.fake()
+const clientValidation = z.object({
+    cpf: z.union([z.number(), z.string()]).transform((val) => {
 
-const clients: Client[] = [ {
+        let numCpf = typeof val === 'string' ? Number(val) : val;
+
+        if (numCpf === 1) {
+            numCpf = Number(cpf.generate());
+        }
+
+        return numCpf;
+    }),
+    name: z.string(),
+    rg: z.number(),
+    cep: z.number(),
+    street: z.string(),
+    neighborhood: z.string(),
+    city: z.string(),
+    state: z.string(),
+    email: z.string().email()
+});
+
+let fakecpf: number = cpf.fake()
+
+const clients: Client[] = [{
     cpf: 1,
     name: 'Carlos',
     rg: 13112332113,
@@ -20,7 +43,7 @@ const clients: Client[] = [ {
     city: 'qqqqqqq',
     state: 'qwwwww',
     email: 'carlos'
-} ];
+}];
 
 clients[0]!.cpf = fakecpf
 
@@ -43,37 +66,24 @@ interface Client extends Pessoa, Endereco {
 };
 
 app.post('/cliente', (req: Request, res: Response) => {
-    const client: Client = (req.body);
+    try {
+        let clientData = req.body;
 
-    if (client.cpf !== undefined && client.cpf !== null &&
-        client.name !== undefined && client.name !== null && client.name !== '' &&
-        client.rg !== undefined && client.rg !== null &&
-        client.cep !== undefined && client.cep !== null &&
-        client.street !== undefined && client.street !== null && client.street !== '' &&
-        client.neighborhood !== undefined && client.neighborhood !== null && client.neighborhood !== '' &&
-        client.city !== undefined && client.city !== null && client.city !== '' &&
-        client.state !== undefined && client.state !== null && client.state !== '' &&
-        client.email !== undefined && client.email !== null && client.email !== ''
-    ) {
-
-        let newcpf: number = 1;
-
-        if (client.cpf === 1) {
-            newcpf = cpf.fake();
-        };
-
-        validate(newcpf);
-
-        if (newcpf) {
-            client.cpf = newcpf
+        if (clientData.cpf === 1) {
+            clientData.cpf = cpf.fake();
         }
-        clients.push(client);
-        console.log(clients)
-        return res.send('Cliente cadastrado com sucesso.');
-    } else {
-        return res.send('Dados inválidos e/ou faltando.');
-    }
 
+        const client = clientValidation.parse(clientData);
+
+        validate(client.cpf);
+
+        clients.push(client);
+
+        console.log(clients);
+        return res.send('Cliente cadastrado com sucesso.');
+    } catch (err) {
+        return res.status(400).send('Dados inválidos e/ou faltando.');
+    }
 });
 
 app.get('/cliente', (req: Request, res: Response) => {
